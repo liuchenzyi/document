82084f066683899b67242f46aedfaf58673098ea
刘臣 <2508871010@qq.com>
2024-12-04 12:57:15 +0800

    解决部分ts 类型报错

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,14 +1,16 @@
 import DefaultTheme from 'vitepress/theme';
 import 'viewerjs/dist/viewer.min.css';
 import imageViewer from 'vitepress-plugin-image-viewer';
+
 import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
 import codeblocksFold from 'vitepress-plugin-codeblocks-fold'; // 导入方法
 import 'vitepress-plugin-codeblocks-fold/style/index.css'; // 导入样式
 import { useRoute,useData } from 'vitepress';
+import type {EnhanceAppContext} from "vitepress/client";
 
 export default {
     ...DefaultTheme,
-    enhanceApp(ctx) {
+    enhanceApp(ctx:EnhanceAppContext) {
         DefaultTheme.enhanceApp(ctx);
         // 注册全局组件，如果你不想使用也可以不添加
         ctx.app.component('vImageViewer', vImageViewer);
@@ -17,13 +19,13 @@ export default {
     setup() {
         // 获取路由
         const route = useRoute();
-        // 使用
+        // 使用 imageViewer 插件
         imageViewer(route);
 
         // 获取前言和路由
         const { frontmatter } = useData();
        
-        // 基础使用
+        // codeblocks 代码折叠插件
         codeblocksFold({ route, frontmatter }, true, 200);
     }
 };
---
+++ types/vitepress-plugin-image-viewer.d.ts
@@ -0,0 +1,6 @@
+declare module 'vitepress-plugin-image-viewer/lib/vImageViewer.vue' {
+
+    import type { DefineComponent } from "vue";
+    const vueComponent: DefineComponent<{}, {}, any>;
+    export default vueComponent;
+}
