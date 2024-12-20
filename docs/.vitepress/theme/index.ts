b2c55ab84e932ad045df6c67c2d8be756dfcd78b
刘臣 <2508871010@qq.com>
2024-12-20 23:55:34 +0800

    修复 page-find 问题

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -7,6 +7,7 @@ import { useData } from 'vitepress'
 
 import type {EnhanceAppContext} from "vitepress/client";
 
+import { setup } from '@css-render/vue3-ssr'
 import { NConfigProvider } from 'naive-ui'
 import { defineComponent, h, onMounted } from 'vue'
 const { Layout } = DefaultTheme
@@ -39,15 +40,22 @@ export default {
     enhanceApp(ctx:EnhanceAppContext) {
         DefaultTheme.enhanceApp(ctx);
 
-        // 注册全局组件，如果你不想使用也可以不添加
-        ctx.app.component('ArticleHeader', ArticleHeader);
-    },
-    setup() {
-        // 获取路由
-        const route = useRoute();
-        // 使用 imageViewer 插件
-        imageViewer(route);
+		// 注册全局组件，如果你不想使用也可以不添加
+		// ctx.app.component('vImageViewer', vImageViewer);
+		ctx.app.component('ArticleHeader', ArticleHeader);
 
+		if (import.meta.env.SSR) {
+			const { collect } = setup(ctx.app)
+			ctx.app.provide('css-render-collect', collect)
+		}
+	},
+	setup() {
+		// 获取路由
+		const route = useRoute();
+		// 使用 imageViewer 插件
+		imageViewer(route);
+
+		// const { isDark } = useData()
 
 		onMounted(() => {
 			// 获取当前页面的标题
