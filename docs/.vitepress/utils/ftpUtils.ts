e3445bab2a4c2d8e543d1806943360308513caac
刘臣 <2508871010@qq.com>
2024-12-25 14:14:10 +0800

    添加 basic-ftp 测试使用 ftp 部署

---
+++ docs/.vitepress/utils/ftpUtils.ts
@@ -0,0 +1,27 @@
+import {Client} from 'basic-ftp'
+
+// console.log('start',Client)
+
+// 测试 FTP
+
+const test = async () => {
+    const client = new Client()
+    client.ftp.verbose = true  //所有套接字通信的调试级日志记录
+    try {
+        await client.access({
+            host: "server",
+            // user: "very",
+            // password: "password",
+            // secure: true
+        })
+        // console.log(await client.list())
+        // await client.downloadTo("local.config", "output/web.config")
+        // await client.uploadFrom("local.config", "output/web-copy.config")
+        await client.uploadFromDir("../../dist", "output/dist2")
+    } catch (err) {
+        console.log(err)
+    }
+    client.close()
+}
+
+test()
