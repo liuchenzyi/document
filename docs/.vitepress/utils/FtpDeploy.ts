cfe4a251d9c1a79ea08fbcce994d55988bdf11a2
刘臣 <2508871010@qq.com>
2024-12-30 20:26:00 +0800

    progress log

--- docs/.vitepress/utils/FtpDeploy.ts
+++ docs/.vitepress/utils/FtpDeploy.ts
@@ -1,11 +1,33 @@
 import { Client } from 'basic-ftp'
 
-
+import fs from 'fs'
+import path  from 'path'
 // 测试 FTP
+const frames =  ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏']
 
+const log = (content: string) => {
+	process.stdout.clearLine(0);  // 清除当前行
+	process.stdout.cursorTo(0);   // 将光标移回到行首
+	process.stdout.write(content );  // 每次从最右侧输出
+}
+
+let count = 0
 const test = async () => {
 	const client = new Client()
-	client.ftp.verbose = true  //所有套接字通信的调试级日志记录
+	client.ftp.verbose = false  //所有套接字通信的调试级日志记录
+
+	const size = getDirSize('../../dist')
+	const sizeStr = formatSize(size)
+	client.trackProgress(info => {
+		// console.log("File", info.name)
+		// console.log("Type", info.type)
+		// console.log("Transferred", info.bytes)
+		// console.log("Transferred Overall", info.bytesOverall)
+		// console.log(`已上传 ${formatSize(info.bytesOverall)},共 ${sizeStr} 进度 ${ (info.bytesOverall/size).toFixed(2) }`, )
+		// 清空上一次输出的内容
+
+		log(`${frames[count++ % frames.length]} Uploading... ${info.name} `)
+	})
 	try {
 		await client.access({
 			host: "server",
@@ -16,9 +38,15 @@ const test = async () => {
 		// console.log(await client.list())
 		// await client.downloadTo("local.config", "output/web.config")
 		// await client.uploadFrom("local.config", "output/web-copy.config")
-		// await client.uploadFromDir("../../dist", "output/dist2")
 
-		await client.rename('output/dist2', 'output/dist3')
+
+
+		await client.uploadFromDir("../../dist", "output/dist2")
+
+
+		log('√ finish upload')
+
+		// await client.rename('output/dist2', 'output/dist3')
 
 	} catch(err) {
 		console.log(err)
@@ -26,7 +54,7 @@ const test = async () => {
 	client.close()
 }
 
-test()
+
 
 
 interface Option {
@@ -101,3 +129,40 @@ class FtpDeploy {
 		}
 	}
 }
+
+// 获取文件夹内所有文件大小，包含子文件夹里边的内容
+const getDirSize = (dirPath: string) => {
+	const files = fs.readdirSync(dirPath)
+	let size = 0
+	files.forEach(file => {
+		const filePath = path.join(dirPath, file)
+		const stat = fs.statSync(filePath)
+		if(stat.isFile()) {
+			size += stat.size
+		}else if(stat.isDirectory()) {
+			size += getDirSize(filePath)
+		}
+	})
+	return size
+}
+
+
+// 格式化大小字符串，按照 b kb mb gb tb 的单位进行格式化
+const formatSize = (size: number) => {
+	if(size < 1024) {
+		return `${size} b`
+	}
+	if(size < 1024 ** 2) {
+		return `${(size / 1024).toFixed(2)} Kb`
+	}
+	if(size < 1024 ** 3) {
+		return `${(size / 1024 ** 2).toFixed(2)} Mb`
+	}
+	if(size< 1024 ** 4){
+		return `${(size / 1024 ** 3).toFixed(2)} Gb`
+	}
+	return `${(size / 1024 ** 4).toFixed(2)} Tb`
+}
+
+test()
+log('\x1B[32m ✓  \x1B[36m  3065 modules transformed.')
