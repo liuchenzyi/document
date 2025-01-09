a38b794c1213b0a34c44ea9605c3b12363616cbb
刘臣 <2508871010@qq.com>
2025-01-09 18:06:03 +0800

    文档发布时间文档更新

--- docs/.vitepress/utils/getPublishTime.ts
+++ docs/.vitepress/utils/getPublishTime.ts
@@ -1,9 +1,9 @@
-import { glob } from 'glob';
+import {glob} from 'glob';
 import path from 'path';
-import { fileURLToPath } from 'url';
+import {fileURLToPath} from 'url';
 import matter from 'gray-matter';
 import fs from 'fs';
-import { execSync } from 'child_process';
+import {execSync} from 'child_process';
 import dayjs from "dayjs";
 // 获取当前文件的绝对路径
 const __filename = fileURLToPath(import.meta.url);
@@ -15,25 +15,14 @@ const __dirname = path.dirname(__filename);
 const directoryPath = path.resolve(__dirname, '../../../docs'); // 根据实际情况调整路径
 
 
-
-/**
- * Writes content to a specified file.
- *
- * This function uses the fs.writeFile method to write a given string content to a specified file path.
- * If the write operation is successful, it logs a success message to the console; if an error occurs,
- * it logs an error message to the console.
- *
- * @param filePath The path of the file to write. If the file does not exist, it will be created.
- * @param content The content to write to the file, which must be a string.
- */
 const writeToFile = (filePath: string, content: string) => {
-	fs.writeFile(filePath, content, 'utf8', err => {
-		if (err) {
-			console.error(`Error writing file ${filePath}:`, err);
-		} else {
-			console.log(`File ${filePath} has been updated.`);
-		}
-	});
+    fs.writeFile(filePath, content, 'utf8', err => {
+        if (err) {
+            console.error(`Error writing file ${filePath}:`, err);
+        } else {
+            console.log(`The published  time had been added to ${filePath}`);
+        }
+    });
 }
 
 
@@ -44,93 +33,77 @@ const writeToFile = (filePath: string, content: string) => {
  * @param filePath 文件路径
  * @returns 返回文件的首次提交时间或创建时间，格式为YYYY-MM-DD HH:mm:ss
  */
-const getFirstCommitTime =  (filePath: string) => {
-	try {
-		const date = execSync(
-			`git log --reverse --format=%ai -- ${ filePath }`,
-			{
-				encoding: 'utf8'
-			}
-		)
-			.split('\n')[0].trim()
+const getFirstCommitTime = (filePath: string) => {
+    try {
+        const [date = ''] = execSync(
+            `git log --reverse --format=%ai -- ${filePath}`,
+            {
+                encoding: 'utf8'
+            }
+        ).split('\n')
 
-		if(date) {
-			return dayjs(date).format('YYYY-MM-DD HH:mm:ss')
-		} else {
-			// 获取本地文件创建时间
-			return getFileBirthtime(filePath)
-		}
-	} catch(error ) {
-		console.error(`Error getting first commit for file ${filePath}:`, error);
-		throw error;
-	}
+        if (date) {
+            return dayjs(date).format('YYYY-MM-DD HH:mm:ss')
+        } else {
+            // 获取本地文件创建时间
+            return getFileBirthtime(filePath)
+        }
+    } catch (error) {
+        console.error(`Error getting first commit for file ${filePath}:`, error);
+        throw error;
+    }
 }
 /**
  * 获取文件的创建时间
- * 在某些系统上，文件的创建时间（birthtime）可能无法正确获取，因此使用访问时间（atime）作为替代
+ * 在某些系统上，文件的创建时间（birthtime）可能无法正确获取，因此使用当前时间作为为替代
  *
  * @param filePath 文件路径
  * @param format 日期格式，默认为 'YYYY-MM-DD HH:mm:ss'
  * @returns 格式化后的日期字符串
  */
 const getFileBirthtime = (filePath: string, format = 'YYYY-MM-DD HH:mm:ss'): string => {
-	const stat = fs.statSync(filePath);
-	// 在一些系统下无法获取birthtime属性的正确时间，使用atime代替
-	const date = stat.birthtime.getFullYear() !== 1970 ? stat.birthtime : stat.atime;
-	return dayjs(date).format(format)
+    try {
+        const stat = fs.statSync(filePath);
+        // 在一些系统下无法获取birthtime属性的正确时间，使用当前时间代替
+        if (stat.birthtime.getFullYear() === 1970) {
+            return dayjs().format(format)
+        }
+        return dayjs(stat.birthtime).format(format)
+    } catch (e) {
+        console.error(`Error getting birthtime`, e)
+        return dayjs().format(format)
+    }
 }
 
-/**
- * 从内容字符串中获取 Front Matter 数据
- *
- * 该函数使用 `matter` 库处理内容字符串，提取其中的 Front Matter 数据
- * Front Matter 通常用于在文件顶部存储元数据，如标题、日期、作者等
- *
- * @param content 文件内容字符串，包含 Front Matter
- * @returns 返回提取的 Front Matter 数据
- */
-const getFrontMatter = (content: string) => {
-	const {data} = matter(content)
-	return data
-}
 /**
  * 用于处理指定目录下的所有Markdown文件，并为缺少日期信息的文件添加日期
  * @param directoryPath 指定的目录路径
  */
-const main = async(directoryPath: string) => {
+const main = async (directoryPath: string) => {
 
-	// 使用 glob 模块获取所有 Markdown 文件
+    // 使用 glob 模块获取所有 Markdown 文件
 
-	const files = await glob('**/*.md', {cwd: directoryPath})
+    const files = await glob('**/*.md', {cwd: directoryPath})
 
-	let i = 0
 
-	console.log(`共 找到 ${files.length} 个 md 文件`)
+    files.forEach(file => {
+        const filePath = path.join(directoryPath, file);
 
-	files.forEach(file => {
-		const filePath = path.join(directoryPath, file);
+        const content = fs.readFileSync(filePath, 'utf8');
 
-		const content = fs.readFileSync(filePath, 'utf8');
+        const {data} = matter(content)
 
-		const data = getFrontMatter(content)
+        const {date} = data
 
-		const {date} = data
-
-		console.log(date)
-
-		if(!date) {
-			const date = getFirstCommitTime(filePath)
-			// 将文件创建时间添加 到 front matter 中
-			const newContent = matter.stringify(content, {...data, date})
-
-			writeToFile(filePath, newContent)
-
-			i++
-			console.log(`已成功将 日期 写入${file} 文件 `)
-		}
-	})
-
-	console.log(`已 成功为${i} 个文件添加date 数据`)
+        // 如果文件没有日期信息，则添加日期,有则不添加
+        if (!date) {
+            const date = getFirstCommitTime(filePath)
+            // 将文件创建时间添加 到 front matter 中
+            const newContent = matter.stringify(content, {...data, date})
+            writeToFile(filePath, newContent)
+        }
+    })
 }
 
 main(directoryPath)
+
--- docs/vite-press/doc-publish.md
+++ docs/vite-press/doc-publish.md
@@ -1,27 +1,27 @@
 ---
-title: 自动添加文件发布时间
+title: 自动添加文档发布时间
 date: '2024-12-10 23:25:02'
 description: 自动给文档添加发布时间
 category: "vite-press"
 tags:
   - Git
-  - javascript
   - Node
+  - child_process
+  - gray-matter
 
 ---
 
-# 自动添加文件发布时间
+# 自动添加文档发布时间
 
-背景：写了一些文档，打算根据文档创建时间生成 time-line 页面，但不想手动为每个文件添加其发布时间。
+背景：已经写了一些文档，打算根据文档创建时间生成 time-line 作为首页， 手动为每个文档手动添加发布时间，比较繁琐，打算使用文档的首次提交时间作为文档发布时间。
 
 ## 需要解决的问题
 
-1. 如何拿到文件的创建时间
+1. 如何拿到文档的发布时间
 2. 获取对应目录下所有的 md 文件
 3. 解析文件元数据，在元数据中添加发布时间
-4. 将数据写回源文件
 
-## 1 获取文档创建时间
+## 1 获取文档首次提交时间
 
 我们可以从 git 历史提交记录拿到对应文件的所有提交记录，从中找到第一次提交时间，将其作为文件的创建时间。
 
@@ -50,7 +50,7 @@ git log  --reverse --format=%ai   -- .\docs\front\index.md
 
 - `---reverse`: 反转 git log 输出的顺序，从最早的提交开始显示。
 - `--format=%ai`: 用于格式化显示每次提交的日期
-- `.\docs\front\Cesium-Roam.md`: 指定要查看的文件路径
+- `.\docs\front\index.md`: 指定要查看的文件路径
 
 **部分日期格式参考**
 
@@ -59,38 +59,78 @@ git log  --reverse --format=%ai   -- .\docs\front\index.md
 - %cd 提交日期（默认格式） Mon Jan 2 14:30:00 2023 +0800
 - %ci 提交日期（ISO 格式） 2023-01-02 14:30:00 +0800
 
-### 使用 node 来执行上述操作
-
-我们可以使用 `child_process` 来执行命令
-
-`child_process` 是 Node.js 的一个内置模块，用于创建和管理子进程。通过这个模块，可以在 Node.js 应用中执行外部命令、与子进程通信等。
-
-child_process 模块的主要功能：
-- 执行系统命令：可以运行操作系统级别的命令，并获取输出结果。
-- 创建子进程：可以启动新的进程来执行特定任务，比如运行另一个 JavaScript 文件或任何其他可执行文件。
-- 进程间通信：支持父进程与子进程之间的消息传递。
-- 资源隔离：子进程独立于父进程运行，即使子进程崩溃也不会影响父进程。
+有些文件已经创建了，但是还没有提交，但是期望给其添加发布时间，可以使用使用文件的创建时间或当前时间作为发布时间
 
 ```ts
-import { execSync } from 'child_process';
-const result = execSync(
-    `git log --reverse --format=%ai -- ${ filePath }`,
+import fs from 'fs';
+import dayjs from "dayjs";
+
+/**
+ * 获取文件的创建时间
+ * 在某些系统上，文件的创建时间（birthtime）可能无法正确获取，因此使用当前时间作为为替代
+ *
+ * @param filePath 文件路径
+ * @param format 日期格式，默认为 'YYYY-MM-DD HH:mm:ss'
+ * @returns 格式化后的日期字符串
+ */
+const getFileBirthtime = (filePath: string, format = 'YYYY-MM-DD HH:mm:ss'): string => {
+    try {
+        const stat = fs.statSync(filePath);
+        if (stat.birthtime.getFullYear() === 1970) {
+            return dayjs().format(format)
+        }
+        return dayjs(stat.birthtime).format(format)
+    } catch (e) {
+        console.error(`Error getting birthtime`, e)
+        return dayjs().format(format)
+    }
+}
+```
+
+## 2 使用 node 执行 shell 命令
+
+我们可以使用 `child_process` 来执行 shell 命令
+
+> `child_process` 是 Node.js 的一个内置模块，供了生成子进程的能力，可以用来执行Shell命令，通过这个模块，可以在 Node.js
+> 应用中执行外部命令、与子进程通信等。
+
+**参考代码**
+
+```ts
+import {execSync} from 'child_process';
+
+const filePath = '.\\docs\\front\\index.md'  // 测试时可以使用绝对路径
+
+// 解构 git log 输出的第一行
+const [date = ''] = execSync(
+    `git log --reverse --format=%ai -- ${filePath}`,
     {
         encoding: 'utf8'
     }
-)
+).split('\n')
 
-console.log(result)
+console.log(date)
+
+// 输出结果
+// 2024-12-21 22:31:21 +0800
 ```
 
-**输出**
-```
-2024-12-21 22:31:21 +0800
+## 3 获取对应目录下所有的 md 文件
+
+[glob npm 地址](https://www.npmjs.com/package/glob )
+
+glob 是一个用于文件路径模式匹配的工具，它允许你使用简单的模式（类似于 shell 中的通配符）来匹配文件和目录。通过
+glob，可以轻松地查找符合特定模式的所有文件或目录，而不需要手动遍历整个文件系统。
+
+我们可以使用 glob 来获取对应目录下的所有 md 文件。
+
+安装 glob
+
+```shell [npm]
+npm i glob -D
 ```
 
-## 获取需要添加发布时间的文件
-
-使用 `glob` 模块可以帮助我们快速获取目录下所有 `md`文件
+**参考代码**
 
 ```js
 const files = await glob('**/*.md', {cwd: directoryPath})
@@ -114,55 +154,70 @@ const files = await glob('**/*.md', {cwd: directoryPath})
 ]  
 ```
 
-需要找到目录下所有 .md 文件，这里使用 glob 模块来实现
+`Glob` 详细使用 可以参考
+> [前端工程化之强大的glob语法](https://juejin.cn/post/6876363718578405384)
 
-## 解析与写入元数据
+## 4 解析与写入元数据
+我们需要解析出文件的元数据，并在文件写入发布时间的时候，将这些数据一起写回文件，避免数据丢失，同时，对于已经添加过发布时间的文件，不进行修改。
 
-我们可以使用 `gary-matter` 来解析与输出数据
-我们需要获取文件中的元数据，判断是否已经添加过发布时间，如果没有则添加发布时间
+[gray-matter npm 地址](https://www.npmjs.com/package/gray-matter)
+> gray-matter是一个开源的 `Node.js` 库，用于解析字符串或文件中的前端元数据。它支持YAML、JSON、TOML和CoffeeScript等多种格式的元数据，并且提供了灵活的选项来设置自定义分隔符。
 
-解析元数据：
+`gray-matter`的使用可以参考 npm 上的文档，写的比较详细。
+
+### 4.1 解析元数据
+`gray-matter` 提供 `matter`和`matter.read` 获取元数据， 区别是 `matter` 是解析字符串， `matter.read` 是解析文件。
+由于这里还需要将 `gray-matter` 的元数据添加到文件中需要文件内容，所以这里使用 `matter`方法
 
 ```js
 import matter from 'gray-matter';
+import path from 'path';
 
-const filePath = path.join(directoryPath, path);	// 拼接完整路径
+const filePath = '.\\docs\\front\\index.md'// 文件路径
 
-const content = fs.readFileSync(filePath, 'utf8');	// 以字符串形式读取文件
+const content = fs.readFileSync(filePath, 'utf8');	// 读取文件内容
 
 const {data} = matter(content)	// 解析文件元数据
+console.log(data)
 ```
 
-**输出**
+### 4.2 将元数据转化为字符串
+`gray-matter` 提供了 `matter.stringify` 方法，可以将元数据和内容转化为字符串，添加到给定的字符串中。
 
+下面是一个例子
+```js
+import matter from 'gray-matter';
+console.log(matter.stringify('foo bar baz', {title: 'Home'}));
+// 结果如下
+// ---
+// title: Home
+// ---
+// foo bar baz
 ```
-{}
-```
-
-**写入发布时间**
+我们还需要将这些添加了发布时间的数据写入文件中
 
 ```js
 import fs from 'fs';
 import matter from 'gray-matter';
-const content = fs.readFileSync(filePath, 'utf8');	// 以字符串形式读取文件
+
+const content = fs.readFileSync('.\\docs\\front\\index.md', 'utf8');	// 以字符串形式读取文件
 
 const {data} = matter(content)	// 解析文件元数据
 
 // 将数据添加到文件内容中
-const newContent = matter.stringify(content, {...data, date:'2024-12-10'})
+const newContent = matter.stringify(content, {...data, date: '2024-12-10'})
 
 // 写入文件
 fs.writeFile(filePath, newContent, 'utf8', err => {
 	if (err) {
 		console.error(`Error writing file ${filePath}:`, err);
 	} else {
-		console.log(`File ${filePath} has been updated.`);
+		console.log(`The published  time had been added to ${filePath}`);
 	}
 });
 ```
 
 ## 完整代码
-
 
 <<< ../.vitepress/utils/getPublishTime.ts
 
